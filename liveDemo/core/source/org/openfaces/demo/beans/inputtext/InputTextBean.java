b27a8f17899cfdf71f8fea32f0e5e6625092bcdf
nata <nata@7826287f-d7cb-499f-91f0-9f45672cc948>
2009-05-08 17:41:37 +0000

    added handling HTML tags in the input fields
    git-svn-id: http://svn-new/TeamDev/OpenFaces/trunk@67 7826287f-d7cb-499f-91f0-9f45672cc948

--- liveDemo/core/source/org/openfaces/demo/beans/inputtext/InputTextBean.java
+++ liveDemo/core/source/org/openfaces/demo/beans/inputtext/InputTextBean.java
@@ -1,5 +1,7 @@
 package org.openfaces.demo.beans.inputtext;
 
+import org.apache.commons.lang.StringEscapeUtils;
+
 /**
  * @author Alexei Tymchenko
  */
@@ -34,13 +36,14 @@ public class InputTextBean {
 
     public String getText() {
         StringBuilder contentBuilder = new StringBuilder();
+
         contentBuilder.append("<p>Dear &#160;").
-                append(getClientFirstName()).
+                append(StringEscapeUtils.escapeHtml(getClientFirstName())).
                 append("!</p><p>Each year during the holiday season, we take great pleasure in setting aside our regular work and sending a heartfelt message to all our best friends and customers.").
                 append("</p><p>How joyful we are that this time has come again to extend to you our sincere gratitude because it is good friends and customers like you that make our business possible.").
                 append("</p><p>May your holiday be filled with joy and the coming year be overflowing with all the good things in life.").append("</p><p>Sincerely Yours, &#160;").
-                append(getFirstName()).append(" &#160; ").
-                append(getLastName()).append(".</p>");
+                append(StringEscapeUtils.escapeHtml(getFirstName())).append(" &#160; ").
+                append(StringEscapeUtils.escapeHtml(getLastName())).append(".</p>");
         return contentBuilder.toString();
     }
 }
