9f3cfffa70af8655e256a5039c50f6eea863eea8
nata <nata@7826287f-d7cb-499f-91f0-9f45672cc948>
2009-05-12 15:46:14 +0000

    fixed button size in text customization option (IE)
    git-svn-id: http://svn-new/TeamDev/OpenFaces/trunk@80 7826287f-d7cb-499f-91f0-9f45672cc948

--- liveDemo/core/source/org/openfaces/demo/beans/twolistselection/TLSStylingBean.java
+++ liveDemo/core/source/org/openfaces/demo/beans/twolistselection/TLSStylingBean.java
@@ -19,7 +19,8 @@ import java.util.List;
 public class TLSStylingBean implements Serializable {
     private static final String DEFAULT_STYLE = "width: 100%;";
     private static final String STYLE = "width: 100%; background-color: #ffffff;";
-    private static final String BUTTON_STYLE = "font-family: Tahoma; color: #005ca3; width: 90px; font-size: 10px;";
+    private static final String BUTTON_STYLE = "font-family: Tahoma; color: #005ca3; width: 90px; font-size: 10px;";    
+    private static final String BUTTON_TEXTS_CUSTOMIZATION_STYLE = "width:131px;";    
     private static final String CAPTION_STYLE = "font-family: Tahoma; background-color: #ffffff; color: #ff8a01; white-space: nowrap;";
     private static final String LIST_STYLE = "font-family: Tahoma; background-color: #e5e5e5; color: #005ca3;";
 
@@ -100,7 +101,11 @@ public class TLSStylingBean implements Serializable {
     }
 
     public String getButtonStyle() {
-        return selectedCustomizations.contains(STYLES_CUSTOMIZATION) ? BUTTON_STYLE : null;
+        return selectedCustomizations.contains(STYLES_CUSTOMIZATION)
+                ? BUTTON_STYLE
+                : (selectedCustomizations.contains(BUTTON_TEXTS_CUSTOMIZATION)
+                    ? BUTTON_TEXTS_CUSTOMIZATION_STYLE
+                    : null);
     }
 
     public String getStyle() {
