d76d0261a30b41c769ae206ef4cb32b51b61fa05
Dallas <Dallas@7826287f-d7cb-499f-91f0-9f45672cc948>
2009-07-18 17:44:11 +0000

    Renaming client-side event attributes in progress...
    git-svn-id: http://svn-new/TeamDev/OpenFaces/trunk@271 7826287f-d7cb-499f-91f0-9f45672cc948

--- openFaces/source/org/openfaces/renderkit/timetable/dayTable.js
+++ openFaces/source/org/openfaces/renderkit/timetable/dayTable.js
@@ -570,8 +570,8 @@ O$._initDayTable = function(componentId,
     event.backgroundElement = eventElement._backgroundElement;
 
     function canEventBeDropppedHere(event) {
-      var startTime = event._start.getTime();
-      var endTime = event._end.getTime();
+      var startTime = event.startDate.getTime();
+      var endTime = event.endDate.getTime();
       var resourceId = event.resourceId;
       for (var i = 0, count = dayTable._dayEvents.length; i < count; i++) {
         var currEvent = dayTable._dayEvents[i];
@@ -582,8 +582,8 @@ O$._initDayTable = function(componentId,
         if (currEvent.resourceId && resourceId && currEvent.resourceId != resourceId)
           continue;
         var timeSpansIntersect =
-                currEvent._end.getTime() > startTime &&
-                currEvent._start.getTime() < endTime;
+                currEvent.endDate.getTime() > startTime &&
+                currEvent.startDate.getTime() < endTime;
         if (timeSpansIntersect)
           return false;
       }
@@ -596,8 +596,8 @@ O$._initDayTable = function(componentId,
       eventElement._updateDropAllowed = function() {
         var dropAllowed = canEventBeDropppedHere(event);
         if (dropAllowed) {
-          eventElement._lastValidStart = event._start;
-          eventElement._lastValidEnd = event._end;
+          eventElement._lastValidStart = event.startDate;
+          eventElement._lastValidEnd = event.endDate;
           eventElement._lastValidResourceId = event.resourceId;
         }
         eventElement._setDropAllowed(dropAllowed);
@@ -618,8 +618,8 @@ O$._initDayTable = function(componentId,
         dayTable._resetScrollingCache();
         eventElement._bringToFront();
         O$.startDragAndDrop(e, this);
-        eventElement._initialStart = eventElement._lastValidStart = event._start;
-        eventElement._initialEnd = eventElement._lastValidEnd = event._end;
+        eventElement._initialStart = eventElement._lastValidStart = event.startDate;
+        eventElement._initialEnd = eventElement._lastValidEnd = event.endDate;
         eventElement._initialResourceId = eventElement._lastValidResourceId = event.resourceId;
         eventElement._originalCursor = O$.getElementStyleProperty(eventElement, "cursor");
         eventElement._dropAllowed = true;
@@ -648,9 +648,9 @@ O$._initDayTable = function(componentId,
             eventUpdated = true;
           }
         }
-        var timeIncrement = newStartTime.getTime() - event._start.getTime();
+        var timeIncrement = newStartTime.getTime() - event.startDate.getTime();
         if (timeIncrement != 0) {
-          var newEndTime = O$.dateByTimeMillis(event._end.getTime() + timeIncrement);
+          var newEndTime = O$.dateByTimeMillis(event.endDate.getTime() + timeIncrement);
           event.setStart(newStartTime);
           event.setEnd(newEndTime);
           eventUpdated = true;
@@ -690,8 +690,8 @@ O$._initDayTable = function(componentId,
             eventElement._setDropAllowed(true);
             eventElement.style.cursor = eventElement._originalCursor;
 
-            if (event._start.getTime() >= dayTable._endTime.getTime() ||
-                event._end.getTime() <= dayTable._startTime.getTime()) {
+            if (event.startDate.getTime() >= dayTable._endTime.getTime() ||
+                event.endDate.getTime() <= dayTable._startTime.getTime()) {
               dayTable._updateEventElements(true);
             } else {
               event.mainElement._updatePos(false, dragAndDropCancelingPeriod, {
@@ -752,16 +752,16 @@ O$._initDayTable = function(componentId,
           var nearestTimeslot = getNearestTimeslotForPosition(left, top + eventResizeHandleHeight / 2);
           var eventUpdated = false;
           if (this == topResizeHandle) {
-            if (event._end.getTime() - nearestTimeslot.time < shortestEventTimeWhileResizing)
-              nearestTimeslot.time = O$.dateByTimeMillis(event._end.getTime() - shortestEventTimeWhileResizing);
-            if (event._start.getTime() != nearestTimeslot.time) {
+            if (event.endDate.getTime() - nearestTimeslot.time < shortestEventTimeWhileResizing)
+              nearestTimeslot.time = O$.dateByTimeMillis(event.endDate.getTime() - shortestEventTimeWhileResizing);
+            if (event.startDate.getTime() != nearestTimeslot.time) {
               event.setStart(nearestTimeslot.time);
               eventUpdated = true;
             }
           } else {
-            if (nearestTimeslot.time - event._start.getTime() < shortestEventTimeWhileResizing)
-              nearestTimeslot.time = O$.dateByTimeMillis(event._start.getTime() + shortestEventTimeWhileResizing);
-            if (event._end.getTime() != nearestTimeslot.time) {
+            if (nearestTimeslot.time - event.startDate.getTime() < shortestEventTimeWhileResizing)
+              nearestTimeslot.time = O$.dateByTimeMillis(event.startDate.getTime() + shortestEventTimeWhileResizing);
+            if (event.endDate.getTime() != nearestTimeslot.time) {
               event.setEnd(nearestTimeslot.time);
               eventUpdated = true;
             }
@@ -856,8 +856,8 @@ O$._initDayTable = function(componentId,
       var firstDataRow = table.body._getRows()[0];
       var leftColBoundaries = O$.getElementBorderRectangle(firstDataRow._cells[resourceColIndex != undefined ? resourceColIndex : 1], true, dayTable._getLayoutCache());
       var rightColBoundaries = O$.getElementBorderRectangle(firstDataRow._cells[resourceColIndex != undefined ? resourceColIndex : columns.length - 1], true, dayTable._getLayoutCache());
-      var top = getVertOffsetByTime(event._start);
-      var bottom = getVertOffsetByTime(event._end);
+      var top = getVertOffsetByTime(event.startDate);
+      var bottom = getVertOffsetByTime(event.endDate);
       var x1 = leftColBoundaries.getMinX() + (event.type != "reserved" ? eventsLeftOffset : reservedEventsLeftOffset);
       var x2 = rightColBoundaries.getMaxX() - (event.type != "reserved" ? eventsRightOffset : reservedEventsRightOffset);
       if (O$.isExplorer() && O$.isStrictMode() && (resourceColIndex === undefined || resourceColIndex == columns.length - 1)) {
@@ -1258,8 +1258,8 @@ O$._initDayTable = function(componentId,
     var event = {
       name: "",
       resourceId: resourceId,
-      _start: startTime,
-      _end: endTime,
+      startDate: startTime,
+      endDate: endTime,
       color: null,
       description: ""
     };
@@ -1495,10 +1495,10 @@ O$._PreloadedTimetableEvents = function(events) {
     var endTime = end.getTime();
     for (var eventIndex = 0, eventCount = this._events.length; eventIndex < eventCount; eventIndex++) {
       var event = this._events[eventIndex];
-      if (event._end.getTime() < event._start.getTime())
+      if (event.endDate.getTime() < event.startDate.getTime())
         continue;
-      if (event._end.getTime() <= startTime ||
-          event._start.getTime() >= endTime)
+      if (event.endDate.getTime() <= startTime ||
+          event.startDate.getTime() >= endTime)
         continue;
       result.push(event);
     }
@@ -1604,13 +1604,13 @@ O$._initEventEditorDialog = function(dayTableId, dialogId, createEventCaption, e
     var resource = dayTable._getResourceForEvent(event);
     if (dialog._resourceField)
       dialog._resourceField.setValue(resource ? resource.name : "");
-    this._startDateField.setSelectedDate(event._start);
+    this._startDateField.setSelectedDate(event.startDate);
     if (this._endDateField)
-      this._endDateField.setSelectedDate(event._end);
-    var duration = event._end.getTime() - event._start.getTime();
-    setFieldText(this._startTimeField, O$.formatTime(event._start));
+      this._endDateField.setSelectedDate(event.endDate);
+    var duration = event.endDate.getTime() - event.startDate.getTime();
+    setFieldText(this._startTimeField, O$.formatTime(event.startDate));
     if (this._endTimeField)
-      setFieldText(this._endTimeField, O$.formatTime(event._end));
+      setFieldText(this._endTimeField, O$.formatTime(event.endDate));
     this._color = event.color;
     setFieldText(this._descriptionArea, event.description);
     this._deleteButton.style.visibility = mode == "update" ? "visible" : "hidden";
@@ -1773,30 +1773,30 @@ O$._datesEqual = function(date1, date2) {
 O$._initEvent = function(event) {
   event.setStart = function(asDate, asString) {
     if (asDate) {
-      event._start = asDate;
+      event.startDate = asDate;
       event.start = O$.formatDateTime(asDate);
     } else
       if (asString) {
         event.start = asString;
-        event._start = O$.parseDateTime(asString);
+        event.startDate = O$.parseDateTime(asString);
       } else
         throw "event.setStart: either asDate parameter, or asTime parameter should be specified";
   };
   event.setEnd = function(asDate, asString) {
     if (asDate) {
-      event._end = asDate;
+      event.endDate = asDate;
       event.end = O$.formatDateTime(asDate);
     } else
       if (asString) {
         event.end = asString;
-        event._end = O$.parseDateTime(asString);
+        event.endDate = O$.parseDateTime(asString);
       } else
         throw "event.setEnd: either asDate parameter, or asTime parameter should be specified";
   };
   event._copyFrom = function(otherEvent) {
     this.id = otherEvent.id;
-    this.setStart(otherEvent._start, otherEvent.start);
-    this.setEnd(otherEvent._end, otherEvent.end);
+    this.setStart(otherEvent.startDate, otherEvent.start);
+    this.setEnd(otherEvent.endDate, otherEvent.end);
     this.name = otherEvent.name;
     this.description = otherEvent.description;
     this.resourceId = otherEvent.resourceId;
@@ -1815,10 +1815,10 @@ O$._initEvent = function(event) {
     if (scrollingOccured)
       dayTable._resetScrollingCache();*/
   };
-  if (event._start || event.start)
-    event.setStart(event._start, event.start);
-  if (event._end || event.end)
-    event.setEnd(event._end, event.end);
+  if (event.startDate || event.start)
+    event.setStart(event.startDate, event.start);
+  if (event.endDate || event.end)
+    event.setEnd(event.endDate, event.end);
 };
 
 O$._initEventPreview = function(eventPreviewId, dayTableId, showingDelay, popupClass,
