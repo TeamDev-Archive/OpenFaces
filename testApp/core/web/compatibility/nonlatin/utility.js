998f63ebdaeff81e4520f16096f9c8bb5a020614
Shumilina <Shumilina@7826287f-d7cb-499f-91f0-9f45672cc948>
2009-08-21 15:28:45 +0000

    'Copyright' headers added
    git-svn-id: http://svn-new/TeamDev/OpenFaces/trunk@347 7826287f-d7cb-499f-91f0-9f45672cc948

--- testApp/core/web/compatibility/nonlatin/utility.js
+++ testApp/core/web/compatibility/nonlatin/utility.js
@@ -1,6 +1,13 @@
 /*
- * Copyright (c) 1998-2009 TeamDev Ltd. All Rights Reserved.
- * Use is subject to license terms.
+ * OpenFaces - JSF Component Library 2.0
+ * Copyright (C) 2007-2009, TeamDev Ltd.
+ * licensing@openfaces.org
+ * Unless agreed in writing the contents of this file are subject to
+ * the GNU Lesser General Public License Version 2.1 (the "LGPL" License).
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * Please visit http://openfaces.org/licensing/ for more details.
  */
 
 //------------------------------------------------------------------------------------------------------------------
