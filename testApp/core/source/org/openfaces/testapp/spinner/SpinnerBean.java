b43a56a411e6adc0097f4465b6b9c0c5a0a3aee3
natalyz <natalyz@7826287f-d7cb-499f-91f0-9f45672cc948>
2009-11-02 19:07:08 +0000

    spinner test page updated: number format samples
    git-svn-id: http://svn-new/TeamDev/OpenFaces/trunk@433 7826287f-d7cb-499f-91f0-9f45672cc948

--- testApp/core/source/org/openfaces/testapp/spinner/SpinnerBean.java
+++ testApp/core/source/org/openfaces/testapp/spinner/SpinnerBean.java
@@ -12,18 +12,65 @@
 
 package org.openfaces.testapp.spinner;
 
+import java.math.BigDecimal;
+
 /**
  * @author Alexander Golubev
  */
 public class SpinnerBean {
 
-    private Integer value;
+    private int value = 1;
+    private float value1 = 0;
+    private long value2 =10000;
+    private BigDecimal value3 = new BigDecimal(0);
+    private Long value4 = 0L;
+    private Number value5 = null;
 
-    public Integer getValue() {
+    public int getValue() {
         return value;
     }
 
-    public void setValue(Integer value) {
+    public void setValue(int value) {
         this.value = value;
+    }
+
+    public float getValue1() {
+        return value1;
+    }
+
+    public void setValue1(float value1) {
+        this.value1 = value1;
+    }
+
+    public long getValue2() {
+        return value2;
+    }
+
+    public void setValue2(long value2) {
+        this.value2 = value2;
+    }
+
+    public BigDecimal getValue3() {
+        return value3;
+    }
+
+    public void setValue3(BigDecimal value3) {
+        this.value3 = value3;
+    }
+
+    public Long getValue4() {
+        return value4;
+    }
+
+    public void setValue4(Long value4) {
+        this.value4 = value4;
+    }
+
+    public Number getValue5() {
+        return value5;
+    }
+
+    public void setValue5(Number value5) {
+        this.value5 = value5;
     }
 }
