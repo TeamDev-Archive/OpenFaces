47b2fa386b7c6b0962071cbd606d170cc1994b3a
natalyz <natalyz@7826287f-d7cb-499f-91f0-9f45672cc948>
2009-12-02 16:42:13 +0000

    o:selectOneRadio demo stub
    git-svn-id: http://svn-new/TeamDev/OpenFaces/trunk@581 7826287f-d7cb-499f-91f0-9f45672cc948

---
+++ liveDemo/core/source/org/openfaces/demo/beans/selectoneradio/Track.java
@@ -0,0 +1,69 @@
+/*
+ * OpenFaces - JSF Component Library 2.0
+ * Copyright (C) 2007-2009, TeamDev Ltd.
+ * licensing@openfaces.org
+ * Unless agreed in writing the contents of this file are subject to
+ * the GNU Lesser General Public License Version 2.1 (the "LGPL" License).
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * Please visit http://openfaces.org/licensing/ for more details.
+ */
+
+package org.openfaces.demo.beans.selectoneradio;
+
+/**
+ * @author Natalia Zolochevska
+ */
+public class Track {
+
+    private int number;
+    private String writer;
+    private String title;
+    private String length;
+
+    public Track(int number, String writer, String title, String length) {
+        this.number = number;
+        this.writer = writer;
+        this.title = title;
+        this.length = length;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public void setWriter(String writer) {
+        this.writer = writer;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLength() {
+        return length;
+    }
+
+    public void setLength(String length) {
+        this.length = length;
+    }
+
+
+    @Override
+    public String toString() {
+        return new StringBuilder().append(getWriter()).append(" - ").append(getTitle()).append(" (").append(getLength()).append(")").toString();
+    }
+}
