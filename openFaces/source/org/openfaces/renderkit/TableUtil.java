e43ccb5b3203dd7e019e0571c653f982e3f2044a
natalyz <natalyz@7826287f-d7cb-499f-91f0-9f45672cc948>
2009-12-22 13:02:34 +0000

    table utils: obtain header and expression from column
    git-svn-id: http://svn-new/TeamDev/OpenFaces/trunk@638 7826287f-d7cb-499f-91f0-9f45672cc948

--- openFaces/source/org/openfaces/renderkit/TableUtil.java
+++ openFaces/source/org/openfaces/renderkit/TableUtil.java
@@ -20,17 +20,23 @@ import org.openfaces.component.table.TableColumns;
 import org.openfaces.org.json.JSONException;
 import org.openfaces.org.json.JSONObject;
 import org.openfaces.util.ComponentUtil;
+import org.openfaces.util.ReflectionUtil;
 import org.openfaces.util.RenderingUtil;
 import org.openfaces.util.ResourceUtil;
 import org.openfaces.util.StyleUtil;
 
+import javax.el.ValueExpression;
 import javax.faces.component.UIComponent;
+import javax.faces.component.UIOutput;
 import javax.faces.context.FacesContext;
 import javax.faces.context.ResponseWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class TableUtil {
     public static final String DEFAULT_HEADER_SECTION_CLASS = "o_table_header_section";
@@ -112,5 +118,141 @@ public class TableUtil {
         }
         return columns;
     }
+
+    /**
+     * Matches expression of the form:
+     * #{a.b.var.d.c}
+     * #{a.b[var].d.c}
+     * #{a["b"][var].d.c}
+     * #{a['b'][var]d.c}
+     * #{a['b'][var]['d']c}
+     * #{var.a.b}
+     * #{d.c[var]}
+     *
+     * @return Pattern groups with indexes 2 and 7 of which select expression before and after var accordingly
+     */
+    /*private static Pattern getExpressionPattern(String var) {
+        String pattern = "#\\{((\\w([\\w\\.\\[\\]\\'\\'\\\"\\\"])*)*([\\.\\[])|())" + var + "(\\]\\.|\\]|\\.)((([\\w\\.\\[\\]\\'\\'\\\"\\\"])*)*)}";
+        return Pattern.compile(pattern);
+    }*/
+
+    /**
+     * Matches expression of the form:
+     * #{var.a.b}
+     *
+     * @return Pattern group with indexe 2 of which selects expression after var
+     */
+    private static Pattern getExpressionPattern(String var) {
+        String pattern = "#\\{var(\\.)((([\\w\\.])*)+)}";
+        return Pattern.compile(pattern);
+    }
+
+
+    private static boolean expressionContainsVar(String expressionString, String var) {
+        Matcher matcher = getExpressionPattern(var).matcher(expressionString);
+        return matcher.find();
+    }
+
+    public static UIOutput obtainColumOutput(BaseColumn column, String var) {
+        return obtainOutput(column, var);
+    }
+
+    public static String obtainColumHeader(BaseColumn column) {
+        UIComponent component = column.getHeader();
+        return obtainOutputValue(component);
+
+        /*FacesContext context = FacesContext.getCurrentInstance();
+        ResponseWriter responseWriter = context.getResponseWriter();
+        StringWriter content = new StringWriter();
+        ResponseWriter contentWriter = responseWriter.cloneWithWriter(content);
+
+        context.setResponseWriter(contentWriter);
+        try {
+            component.encodeAll(context);
+        } catch (IOException e) {
+            throw new FacesException(e);
+        }
+        context.setResponseWriter(responseWriter);
+        String componentOutput = content.toString();
+        String result = componentOutput.replaceAll("\\<.*?\\>", "");
+        return result;*/
+
+
+    }
+
+    private static UIOutput obtainOutput(UIComponent component, String var) {
+        if (component instanceof UIOutput) {
+            ValueExpression valueExpression = component.getValueExpression("value");
+            if (expressionContainsVar(valueExpression.getExpressionString(), var)) {
+                return (UIOutput) component;
+            }
+        }
+        for (UIComponent child : component.getChildren()) {
+            UIOutput childOutput = obtainOutput(child, var);
+            if (childOutput != null) {
+                return childOutput;
+            }
+        }
+        return null;
+
+    }
+
+    private static String obtainOutputValue(UIComponent component) {
+        if (component instanceof UIOutput) {
+            return String.valueOf(((UIOutput) component).getValue());
+        }
+        for (UIComponent child : component.getChildren()) {
+            String childValue = obtainOutputValue(child);
+            if (childValue != null) {
+                return childValue;
+            }
+        }
+        return null;
+
+    }
+
+    public static Class getFilteredValueType(
+            FacesContext context,
+            AbstractTable table,            
+            ValueExpression expression) {
+        int index = table.getRowIndex();
+        try {
+            table.setRowIndex(0);
+            Class result = expression.getType(context.getELContext());
+            table.setRowIndex(index);
+            return result;
+        } catch (IllegalArgumentException e) {
+            // means that there's no row data and row with index=0
+            String expressionString = expression.getExpressionString();
+            if (expressionString.split("#").length > 1) { //consist of more than one expression
+                return String.class; //than concatenation
+            }
+            String var = table.getVar();
+            ValueExpression rowDataBeanValueExpression = table.getValueExpression("value");
+
+            Class rowDataBeanType = rowDataBeanValueExpression.getType(context.getELContext());
+            Class rowDataClass;
+            if (rowDataBeanType.isArray()) {
+                rowDataClass = rowDataBeanType.getComponentType();
+            } else if (rowDataBeanType.isAssignableFrom(Collection.class)) {
+                rowDataClass = ReflectionUtil.getGenericParameterClass(rowDataBeanType);
+            } else {
+                return Object.class;
+            }
+
+            Matcher expressionMatcher = getExpressionPattern(var).matcher(expressionString);
+            if (!expressionMatcher.find()) {
+                throw new IllegalArgumentException("Unsupported expression: " + expression);
+            }
+
+            //String expressionBeforeVar = expressionMatcher.group(2);
+            /*if (expressionBeforeVar != null) {
+                Class typeOfExpressionBefore = ValueBindings.createValueExpression(context, "#{" + expressionBeforeVar + "}").getType(context.getELContext());
+            }*/
+            String expressionAfterVar = expressionMatcher.group(2);
+            return ReflectionUtil.definePropertyType(rowDataClass, expressionAfterVar);
+        }
+    }
+
 
 }
