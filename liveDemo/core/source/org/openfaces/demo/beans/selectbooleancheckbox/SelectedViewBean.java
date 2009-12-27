24c7609a44e212298adfec416eb5837bea69f8cd
natalyz <natalyz@7826287f-d7cb-499f-91f0-9f45672cc948>
2009-12-27 13:20:01 +0000

    live demo: 'behind the scene' buttons
    git-svn-id: http://svn-new/TeamDev/OpenFaces/trunk@666 7826287f-d7cb-499f-91f0-9f45672cc948

---
+++ liveDemo/core/source/org/openfaces/demo/beans/selectbooleancheckbox/SelectedViewBean.java
@@ -0,0 +1,36 @@
+/*
+ * OpenFaces - JSF Component Library 2.0
+ * Copyright (C) 2007-2009, TeamDev Ltd.
+ * licensing@openfaces.org
+ * Unless agreed in writing the contents of this file are subject to
+ * the GNU Lesser General Public License Version 2.1 (the "LGPL" License).
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * Please visit http://openfaces.org/licensing/ for more details.
+ */
+
+package org.openfaces.demo.beans.selectbooleancheckbox;
+
+public class SelectedViewBean {
+    private static final int ORIGINAL_VIEW_INDEX = 0;
+    private static final int BEHIND_THE_SCHENE_INDEX = 1;
+
+    private int selectedViewIndex = 0;
+
+    public int getSelectedViewIndex() {
+        return selectedViewIndex;
+    }
+
+    public void setSelectedViewIndex(int selectedViewIndex) {
+        this.selectedViewIndex = selectedViewIndex;
+    }
+
+    public boolean isOriginalView() {
+        return selectedViewIndex == ORIGINAL_VIEW_INDEX;
+    }
+
+    public boolean isBefindSceneView() {
+        return selectedViewIndex == BEHIND_THE_SCHENE_INDEX;
+    }
+}
