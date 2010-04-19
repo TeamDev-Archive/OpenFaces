17a818921b03dd44c3e420dd05c98c819bc8ae2d
Dallas <Dallas@7826287f-d7cb-499f-91f0-9f45672cc948>
2010-04-19 12:15:26 +0000

    ColumnVisibilityMenu refactoring
    git-svn-id: http://svn-new/TeamDev/OpenFaces/trunk@900 7826287f-d7cb-499f-91f0-9f45672cc948

--- openFaces/source/org/openfaces/renderkit/table/ColumnVisibilityMenuRenderer.java
+++ openFaces/source/org/openfaces/renderkit/table/ColumnVisibilityMenuRenderer.java
@@ -11,32 +11,73 @@
  */
 package org.openfaces.renderkit.table;
 
+import org.openfaces.component.command.MenuItem;
+import org.openfaces.component.command.PopupMenu;
 import org.openfaces.component.table.AbstractTable;
+import org.openfaces.component.table.BaseColumn;
 import org.openfaces.component.table.ColumnVisibilityMenu;
+import org.openfaces.renderkit.TableUtil;
 import org.openfaces.renderkit.command.PopupMenuRenderer;
+import org.openfaces.renderkit.select.SelectBooleanCheckboxImageManager;
+import org.openfaces.util.Resources;
 import org.openfaces.util.Rendering;
 import org.openfaces.util.ScriptBuilder;
 
+import javax.faces.FacesException;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import java.io.IOException;
+import java.util.List;
 
 public class ColumnVisibilityMenuRenderer extends PopupMenuRenderer {
     @Override
     public void encodeBegin(FacesContext context, UIComponent component) throws IOException {
         ColumnVisibilityMenu cvm = (ColumnVisibilityMenu) component;
-        cvm.updateMenuItems(context);
+        updateMenuItems(context, cvm);
         super.encodeBegin(context, component);
+    }
+
+    private void updateMenuItems(FacesContext context, ColumnVisibilityMenu cvm) {
+        List<UIComponent> menuChildren = cvm.getChildren();
+        menuChildren.clear();
+        AbstractTable table = getTable(cvm);
+        cvm.getAttributes().put(PopupMenuRenderer.ATTR_DEFAULT_INDENT_CLASS, "o_popup_menu_indent o_columnVisibilityMenuIndent");
+
+        List<BaseColumn> visibleColumns = table.getRenderedColumns();
+        List<BaseColumn> allColumns = table.getAllColumns();
+        for (BaseColumn column : allColumns) {
+            MenuItem menuItem = new MenuItem();
+            menuItem.setValue(TableUtil.getColumnHeader(column));
+            boolean columnVisible = visibleColumns.contains(column);
+            menuItem.setIconUrl(Resources.getInternalURL(context,
+                    SelectBooleanCheckboxImageManager.class,
+                    columnVisible
+                            ? SelectBooleanCheckboxImageManager.DEFAULT_SELECTED_IMAGE
+                            : SelectBooleanCheckboxImageManager.DEFAULT_UNSELECTED_IMAGE,
+                    false));
+            menuChildren.add(menuItem);
+        }
+    }
+
+    private AbstractTable getTable(ColumnVisibilityMenu cvm) {
+        UIComponent parent = cvm.getParent();
+        while (parent != null && (parent instanceof MenuItem || parent instanceof PopupMenu))
+            parent = parent.getParent();
+        if (!(parent instanceof AbstractTable))
+            throw new FacesException("<o:columnVisibilityMenu> can only be inserted into the \"columnMenu\" facet of " +
+                    "the <o:dataTable> or <o:treeTable> components (either directly or as its sub-menu).");
+        AbstractTable table = (AbstractTable) parent;
+        return table;
     }
 
     @Override
     public void encodeEnd(FacesContext context, UIComponent component) throws IOException {
         super.encodeEnd(context, component);
         ColumnVisibilityMenu cvm = (ColumnVisibilityMenu) component;
-        AbstractTable table = cvm.getTable();
+        AbstractTable table = getTable(cvm);
         Rendering.renderInitScript(context, new ScriptBuilder().initScript(context,
                 component, "O$.ColumnMenu._initColumnVisibilityMenu", table),
                 AbstractTableRenderer.getTableJsURL(context));
-
+        cvm.getChildren().clear();
     }
 }
