43f6c809eefc8ffe8e2141d62963d7d4ec94ace2
Dmitry Pikhulya <dmitry.pikhulya@teamdev.com>
2010-04-22 20:46:03 +0300

    Added RichFaces4/PrimeFaces 2 libraries

--- openFaces/source/org/openfaces/component/CompoundComponent.java
+++ openFaces/source/org/openfaces/component/CompoundComponent.java
@@ -16,11 +16,8 @@ import javax.faces.context.FacesContext;
 /**
  * This is an interface for OpenFaces components that need to create other subcomponents for rendering their contents.
  * Application developers should pay attention to this interface when creating components programmatically in backing
- * beans. All components that implement this interface should have their createSubComponents method invoked after the
- * component's properties are configured.
- * <p/>
- * When declaring the components in the usual way (with Facelets/JSP tags) no special actions should be taken since
- * createSubComponents is invoked in component's tag handlers automatically in this case.
+ * beans. All components that implement this interface should have their createSubComponents method invoked when a
+ * component is added to the view.
  */
 public interface CompoundComponent {
 
