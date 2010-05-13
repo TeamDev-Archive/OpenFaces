c8d492095f6405ce1a5c803dade84e3810b92990
Dmitry Pikhulya <dmitry.pikhulya@teamdev.com>
2010-05-13 19:19:28 +0300

    Post rebase amendments

--- openFaces/source/org/openfaces/taglib/facelets/chart/SaveChartMenuItemTagHandler.java
+++ openFaces/source/org/openfaces/taglib/facelets/chart/SaveChartMenuItemTagHandler.java
@@ -1,5 +1,5 @@
 /*
- * OpenFaces - JSF Component Library 2.0
+ * OpenFaces - JSF Component Library 3.0
  * Copyright (C) 2007-2010, TeamDev Ltd.
  * licensing@openfaces.org
  * Unless agreed in writing the contents of this file are subject to
@@ -11,9 +11,10 @@
  */
 package org.openfaces.taglib.facelets.chart;
 
-import com.sun.facelets.tag.jsf.ComponentConfig;
 import org.openfaces.taglib.facelets.AbstractFaceletsComponentHandler;
 import org.openfaces.taglib.internal.chart.SaveChartMenuItemTag;
+
+import javax.faces.view.facelets.ComponentConfig;
 
 public class SaveChartMenuItemTagHandler extends AbstractFaceletsComponentHandler {
 
