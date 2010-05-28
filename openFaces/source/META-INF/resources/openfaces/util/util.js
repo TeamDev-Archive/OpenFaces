1ef584a0b37114d29af8b477eeefa52c2660dd96
Dmitry Pikhulya <dmitry.pikhulya@teamdev.com>
2010-05-28 16:52:30 +0300

    Removed deault.css check from util.js, since there's no need to declare ResourceFilter in web.xml now.

--- openFaces/source/META-INF/resources/openfaces/util/util.js
+++ openFaces/source/META-INF/resources/openfaces/util/util.js
@@ -4642,8 +4642,6 @@ if (!window.O$) {
   // ----------------- COMPONENT UTILS -------------------------------------------
 
   O$.addLoadEvent(function() {
-    if (!O$.findCssRule(".o_default_css_marker"))
-      O$.logError("default.css file is not loaded. The usual reason is application misconfiguration. See OpenFaces Installation and Configuration guide (resource filter configuration, etc).");
     O$._loaded = true;
   });
 
