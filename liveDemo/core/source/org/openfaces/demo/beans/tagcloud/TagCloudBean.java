d2ece5ce9d9d76b8948b9fc13ab0c3dcb1841429
Dmitry Pikhulya <dmitry.pikhulya@teamdev.com>
2011-10-03 11:53:59 +0300

    Some tagCloud demo revisions

--- liveDemo/core/source/org/openfaces/demo/beans/tagcloud/TagCloudBean.java
+++ liveDemo/core/source/org/openfaces/demo/beans/tagcloud/TagCloudBean.java
@@ -84,50 +84,51 @@ public class TagCloudBean implements ActionListener, Serializable {
         }
     };
     private List<SelectItem> orderList;
-    private List<SelectItem> weightFormatList;
+    private List<String> weightFormatList;
+
     public TagCloudBean() {
         initEntities();
         setMinFontSize(15);
         setMaxFontSize(50);
-        setMaxColor(colors.getColorByText("SteelBlue") );
-        setMinColor(colors.getColorByText("Tan") );
+        setMaxColor(colors.getColorByText("CornflowerBlue"));
+        setMinColor(colors.getColorByText("YellowGreen"));
     }
 
     private void initEntities() {
         entityStore = new TaggedEntityStore();
         entitiesItemsList = new ArrayList<Item>();
         coloredItemsList = new ArrayList<Item>();
-        coloredItemsList.add(new Item("Java","","#",152,colors.getColorByText("SteelBlue")));
-        coloredItemsList.add(new Item("JSF","","#",105,colors.getColorByText("SteelBlue")));
-        coloredItemsList.add(new Item("AJAX","","#",32,colors.getColorByText("SteelBlue")));
-        coloredItemsList.add(new Item("HTML","","#",104,colors.getColorByText("SteelBlue")));
-        coloredItemsList.add(new Item("JavaScript","","#",43,colors.getColorByText("SteelBlue")));
-        coloredItemsList.add(new Item("J2EE","","#",85,colors.getColorByText("SteelBlue")));
+        coloredItemsList.add(new Item("Java", "", "#", 152, colors.getColorByText("SteelBlue")));
+        coloredItemsList.add(new Item("JSF", "", "#", 105, colors.getColorByText("SteelBlue")));
+        coloredItemsList.add(new Item("AJAX", "", "#", 32, colors.getColorByText("SteelBlue")));
+        coloredItemsList.add(new Item("HTML", "", "#", 104, colors.getColorByText("SteelBlue")));
+        coloredItemsList.add(new Item("JavaScript", "", "#", 43, colors.getColorByText("SteelBlue")));
+        coloredItemsList.add(new Item("J2EE", "", "#", 85, colors.getColorByText("SteelBlue")));
 
-        coloredItemsList.add(new Item("FireFox","","#",120,colors.getColorByText("Tomato")));
-        coloredItemsList.add(new Item("IE","","#",148,colors.getColorByText("Tomato")));
-        coloredItemsList.add(new Item("Opera","","#",50,colors.getColorByText("Tomato")));
-        coloredItemsList.add(new Item("Chrome","","#",47,colors.getColorByText("Tomato")));
-        coloredItemsList.add(new Item("Safari","","#",21,colors.getColorByText("Tomato")));
+        coloredItemsList.add(new Item("FireFox", "", "#", 120, colors.getColorByText("Tomato")));
+        coloredItemsList.add(new Item("IE", "", "#", 148, colors.getColorByText("Tomato")));
+        coloredItemsList.add(new Item("Opera", "", "#", 50, colors.getColorByText("Tomato")));
+        coloredItemsList.add(new Item("Chrome", "", "#", 47, colors.getColorByText("Tomato")));
+        coloredItemsList.add(new Item("Safari", "", "#", 21, colors.getColorByText("Tomato")));
 
-        coloredItemsList.add(new Item("DeskTop","","#",154,colors.getColorByText("DarkOrange")));
-        coloredItemsList.add(new Item("LapTop","","#",132,colors.getColorByText("DarkOrange")));
-        coloredItemsList.add(new Item("NetBook","","#",83,colors.getColorByText("DarkOrange")));
-        coloredItemsList.add(new Item("iPod","","#",54,colors.getColorByText("DarkOrange")));
-        coloredItemsList.add(new Item("Nexen","","#",25,colors.getColorByText("DarkOrange")));
+        coloredItemsList.add(new Item("DeskTop", "", "#", 154, colors.getColorByText("MediumSeaGreen")));
+        coloredItemsList.add(new Item("LapTop", "", "#", 132, colors.getColorByText("MediumSeaGreen")));
+        coloredItemsList.add(new Item("NetBook", "", "#", 83, colors.getColorByText("MediumSeaGreen")));
+        coloredItemsList.add(new Item("iPod", "", "#", 54, colors.getColorByText("MediumSeaGreen")));
+        coloredItemsList.add(new Item("Nexen", "", "#", 25, colors.getColorByText("MediumSeaGreen")));
 
 
         orderList = new ArrayList<SelectItem>();
         for (TagsOrder order : TagsOrder.values()) {
             orderList.add(new SelectItem(order));
         }
-        weightFormatList = new ArrayList<SelectItem>();
-        weightFormatList.add(new SelectItem("(#0)"));
-        weightFormatList.add(new SelectItem("[#0]"));
-        weightFormatList.add(new SelectItem("<#0>"));
-        weightFormatList.add(new SelectItem("-#0-"));
-        weightFormatList.add(new SelectItem("#0"));
-           
+        weightFormatList = new ArrayList<String>();
+        weightFormatList.add("(#0)");
+        weightFormatList.add("[#0]");
+        weightFormatList.add("#0 items");
+        weightFormatList.add("-#0-");
+        weightFormatList.add("#0");
+
         layoutList = new ArrayList<SelectItem>();
         for (Layout layout : Layout.values()) {
             layoutList.add(new SelectItem(layout));
@@ -165,7 +166,7 @@ public class TagCloudBean implements ActionListener, Serializable {
         for (String tag : tags) {
             counter = 0;
             for (TaggedEntity current : this.entityStore.getEntities()) {
-                if ((flag ? true : current.getType().toString().equals(currentCategory)) &&
+                if ((flag || current.getType().toString().equals(currentCategory)) &&
                         current.hasTag(tag)) {
                     counter++;
                 }
@@ -264,7 +265,7 @@ public class TagCloudBean implements ActionListener, Serializable {
 
     public String getMaxItemStyle() {
         StringBuilder rez = new StringBuilder("");
-        if(maxColor != null){
+        if (maxColor != null) {
             rez.append("color:").append(maxColor.getHex()).append(";");
             rez.append("font-size:").append(getMaxFontSize()).append("px;");
         }
@@ -277,7 +278,7 @@ public class TagCloudBean implements ActionListener, Serializable {
 
     public String getMinItemStyle() {
         StringBuilder rez = new StringBuilder("");
-        if(minColor != null){
+        if (minColor != null) {
             rez.append("color:").append(minColor.getHex()).append(";");
             rez.append("font-size:").append(getMinFontSize()).append("px;");
         }
@@ -336,12 +337,12 @@ public class TagCloudBean implements ActionListener, Serializable {
 
     public String getCurrentEntities() {
         boolean flag = currentCategory.equals("All");
-        StringBuilder currentEntitiesBuilder = new StringBuilder("");        
+        StringBuilder currentEntitiesBuilder = new StringBuilder("");
         for (TaggedEntity current : entityStore.getEntities()) {
-            if ((flag ? true : current.getType().toString().equals(currentCategory))) {
+            if ((flag || current.getType().toString().equals(currentCategory))) {
                 if ((var != null && current.hasTag(var.getText()))) {
                     currentEntitiesBuilder.append("<span>Category: ").append(current.getType()).append("</span>");
-                    currentEntitiesBuilder.append(current.getInfo());                    
+                    currentEntitiesBuilder.append(current.getInfo());
                 }
             }
         }
@@ -361,12 +362,8 @@ public class TagCloudBean implements ActionListener, Serializable {
         return orderList;
     }
 
-    public List<SelectItem> getWeightFormatList() {
+    public List<String> getWeightFormatList() {
         return weightFormatList;
-    }
-
-    public void setWeightFormatList(List<SelectItem> weightFormatList) {
-        this.weightFormatList = weightFormatList;
     }
 
     public Converter getOrderConverter() {
@@ -394,8 +391,8 @@ public class TagCloudBean implements ActionListener, Serializable {
         setMinFontSize(15);
         setMaxFontSize(50);
         setItemWeightFormat("(#0)");
-        setMaxColor(colors.getColorByText("SteelBlue") );
-        setMinColor(colors.getColorByText("Tan") );
+        setMaxColor(colors.getColorByText("SteelBlue"));
+        setMinColor(colors.getColorByText("Tan"));
         setItemWeightVisible(false);
 //        setMaxColor(colors.getColorByText("AntiqueWhite") );
 //        setMinColor(colors.getColorByText("PaleTurquoise") );
@@ -410,9 +407,9 @@ public class TagCloudBean implements ActionListener, Serializable {
         setMinFontSize(12);
         setMaxFontSize(30);
         setItemWeightVisible(true);
-        setMaxColor(colors.getColorByText("Peru") );
-        setMinColor(colors.getColorByText("MidnightBlue") );
-         setItemWeightFormat("-#0-");
+        setMaxColor(colors.getColorByText("Peru"));
+        setMinColor(colors.getColorByText("MidnightBlue"));
+        setItemWeightFormat("-#0-");
     }
 
     public void setOvalTheme(ActionEvent event) {
@@ -421,20 +418,21 @@ public class TagCloudBean implements ActionListener, Serializable {
         setOrder(TagsOrder.WEIGHT);
         setMinFontSize(15);
         setMaxFontSize(25);
-        setMaxColor(colors.getColorByText("DarkTurquoise") );
-        setMinColor(colors.getColorByText("Tomato") );
+        setMaxColor(colors.getColorByText("DarkTurquoise"));
+        setMinColor(colors.getColorByText("Tomato"));
         setVertical(false);
         setOval(true);
         setItemWeightVisible(false);
         setItemWeightFormat("<#0>");
     }
+
     public void setSphereTheme(ActionEvent event) {
         setSkin("sphere");
         setLayout(Layout.SPHERE);
         setMinFontSize(15);
         setMaxFontSize(25);
-        setMaxColor(colors.getColorByText("DodgerBlue") );
-        setMinColor(colors.getColorByText("SteelBlue") );
+        setMaxColor(colors.getColorByText("DodgerBlue"));
+        setMinColor(colors.getColorByText("SteelBlue"));
         setVertical(false);
         setOval(false);
         setItemWeightVisible(false);
