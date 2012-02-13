5be003ab061dc1b7c23809bedfb40fc376dec4b1
Andrii Loboda <reyadovic@gmail.com>
2012-02-13 13:57:11 +0200

    SingleFileUpload component - fixed work of component inside table in Chrome and Opera.

--- openFaces/source/org/openfaces/renderkit/input/SingleFileUploadRenderer.java
+++ openFaces/source/org/openfaces/renderkit/input/SingleFileUploadRenderer.java
@@ -100,11 +100,11 @@ public class SingleFileUploadRenderer extends AbstractFileUploadRenderer {
 
     @Override
     protected void writeProgressBar(FacesContext context) throws IOException {
-        if (progressBar == null) {
+       if (progressBar == null) {
             progressBar = new ProgressBar();
-            progressBar.setStyleClass("o_s_fileup_pro_bar");
-            progressBar.setLabelStyle("display:none");
         }
+        progressBar.setStyleClass(Styles.getCSSClass(context, progressBar, progressBar.getStyle(), StyleGroup.regularStyleGroup(), progressBar.getStyleClass(), "o_s_fileup_pro_bar"));
+        progressBar.setLabelClass(Styles.getCSSClass(context, progressBar, progressBar.getLabelStyle(), StyleGroup.regularStyleGroup(), progressBar.getLabelClass(), "o_s_file_upload_progress_label"));
         progressBar.encodeAll(context);
     }
 
