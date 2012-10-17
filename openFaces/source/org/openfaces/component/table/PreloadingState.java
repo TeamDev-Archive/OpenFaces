96fba9cfde94fd19fb1d390722a3ded090f4652d
Vladislav Lubenskiy <vladislav.lubenskiy@teamdev.com>
2012-10-17 17:14:40 +0300

    Fixed node collapsing behavior (OF-192)

---
+++ openFaces/source/org/openfaces/component/table/PreloadingState.java
@@ -0,0 +1,33 @@
+/*
+ * OpenFaces - JSF Component Library 2.0
+ * Copyright (C) 2007-2012, TeamDev Ltd.
+ * licensing@openfaces.org
+ * Unless agreed in writing the contents of this file are subject to
+ * the GNU Lesser General Public License Version 2.1 (the "LGPL" License).
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * Please visit http://openfaces.org/licensing/ for more details.
+ */
+
+package org.openfaces.component.table;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This class stores TreePath'es of preloaded nodes.
+ * @author Vladislav Lubenskiy
+ */
+class PreloadingState {
+    private List<TreePath> preloadedNodes = new ArrayList<TreePath>();
+
+    public void addPreloadedTreePath(TreePath path) {
+        if (!preloadedNodes.contains(path))
+            preloadedNodes.add(path);
+    }
+
+    public boolean isPreloaded(TreePath path) {
+        return preloadedNodes.contains(path);
+    }
+}
